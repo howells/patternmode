5855f25b4850458d4ffd4737595a8211c0143a53
Daniel Howells <daniel.howells@gmail.com>
2025-08-25 09:09:25 +0100

    .

--- packages/ui/src/components/bar-list/bar-list.playwright.test.tsx
+++
@@ -1,9 +0,0 @@
-import { expect, test } from "@playwright/experimental-ct-react";
-import { TestBarList } from "./bar-list.stories";
-
-test.describe("Simple BarList Test", () => {
-	test("should render bar-list", async ({ mount }) => {
-		const component = await mount(<TestBarList />);
-		await expect(component).toBeVisible();
-	});
-});
